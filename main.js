9c3c29a395e5839c94408a3607f69be14a8f2eee
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2021-01-16 16:33:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 img="";
+status="";
 function preload(){
     img=loadImage("dog_cat.jpg");
 }
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
 }
+function modelloaded(){
+    console.log("Model Loaded");
+    status= true;
+    objectDetector.detect(img,getResult);
+} 
+function getResult(error,results){
+   if(error){
+       console.log(error);
+   }else{
+       console.log(results);
+   }
+}
+
 function draw(){
     image(img,0,0,640,420);
 
@@ -15,4 +31,11 @@ function draw(){
     noFill();
     stroke("red");
     rect(30,60,350,300)
+
+    fill("blue");
+    textSize(20);
+    text("Cat",280,140);
+    noFill();
+    stroke("blue");
+    rect(270,60,300,300)
 }
